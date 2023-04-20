ce569a0dea567d8fb551d5cf3490fdb16eb8b197
Karolis Jasadavičius <karolisjasad@gmail.com>
2023-04-20 14:33:05 +0300

    Pradinis pakeitimas

---
+++ biudzetas_bendras_projektas.py
@@ -0,0 +1,21 @@
+# Komandinis darbas - biudžeto programėlė
+
+# savybės: suma ir įrašas
+class Irasas():
+    pass
+
+# savybė: gavejas
+class Irasas(Irasas):
+    pass
+
+# savybė: siuntejas
+class Pajamos(Irasas):
+    pass
+
+# savybe: zurnalas: šarašas įrašų
+# turi turėti metodus:
+# Ataskaitai, balansui
+# pajamų įrašo sukūrimas ir įtraukimui į žurnalą
+# išlaidų įrašo sukūrimas ir įtraukimui į žurnalą
+class Biudzetas():
+    pass
