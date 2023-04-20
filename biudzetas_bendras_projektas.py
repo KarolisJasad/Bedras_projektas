39de0ec233249c354ae6a72fd3a23f1f6df2d9a7
Karolis Jasadavičius <karolisjasad@gmail.com>
2023-04-20 15:44:20 +0300

    Sukurtos klases.

--- biudzetas_bendras_projektas.py
+++ biudzetas_bendras_projektas.py
@@ -1,21 +1,53 @@
 # Komandinis darbas - biudžeto programėlė
 
-# savybės: suma ir komentara
+# savybės: suma ir komentaras
 class Irasas():
-    pass
+    def __init__(self, suma, komentaras):
+        self.suma = suma
+        self.komentaras = komentaras
 
 # savybė: gavejas
 class Islaidos(Irasas):
-    pass
+    def __init__(self, suma, komentaras, gavejas):
+        super().__init__(suma, komentaras)
+        self.gavejas = gavejas
+
 
 # savybė: siuntejas
 class Pajamos(Irasas):
-    pass
+    def __init__(self, suma, komentaras, siuntejas):
+        super().__init__(suma, komentaras)
+        self.siuntejas = siuntejas
 
 # savybe: zurnalas: sąrašas įrašų
-# turi turėti metodus:
+# turi turėti metodus:**
 # Ataskaitai, balansui
 # pajamų įrašo sukūrimas ir įtraukimui į žurnalą
 # išlaidų įrašo sukūrimas ir įtraukimui į žurnalą
 class Biudzetas():
-    pass
+    def __init__(self):
+        self.sarasas = []
+
+    def pajamu_sukurimas(self, pajamos):
+        self.sarasas.append(pajamos)
+
+    def islaidu_sukurimas(self, islaidos):
+        self.sarasas.append(islaidos)
+
+    def info_ataskaita(self,):
+        pass
+
+biudzetas = Biudzetas()
+pajamos = Pajamos(500, "pajamos", siuntejas="tadas")
+biudzetas.pajamu_sukurimas(pajamos)
+
+for pajamos in biudzetas.sarasas:
+    print(pajamos.siuntejas, "pajamos yra:", pajamos.suma)
+
+biudzetas = Biudzetas()
+islaidos = Islaidos(200, "islaidos", gavejas="petras")
+biudzetas.islaidu_sukurimas(islaidos)
+
+for islaidos in biudzetas.sarasas:
+    print(islaidos.gavejas, "islaidos yra:", islaidos.suma)
+
